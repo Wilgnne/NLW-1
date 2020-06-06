75ae86d396c8fb6a3160f57a151f9ae3133705e8
Wilgnne <wilgnne.kba@gmail.com>
2020-06-06 17:55:40 +0000

    Updates

---
+++ precache-manifest.dd3d4ab9e44b43ec8eadd61aaf4ad5c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e34721ae43833b1554234b482630d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cd35ed1910cb84945a8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4782e1f47ab8f5416ec",
+    "url": "/static/js/2.98c95162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.98c95162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd35ed1910cb84945a8",
+    "url": "/static/js/main.bf45a256.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
