bf107c1e7e002aa9e677c01d60218e29612fee23
Wilgnne <wilgnne.kba@gmail.com>
2020-06-06 23:50:26 +0000

    Updates

---
+++ precache-manifest.beed73c0c43d401b1477e053dfa2b6ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66ea52ce1ef26be012340fdf3313dee",
+    "url": "https://wilgnne.github.io/nlw-1/index.html"
+  },
+  {
+    "revision": "e24f09e3c3793d70e407",
+    "url": "https://wilgnne.github.io/nlw-1/static/css/main.525ba840.chunk.css"
+  },
+  {
+    "revision": "56bd5170590fe42279d5",
+    "url": "https://wilgnne.github.io/nlw-1/static/js/2.1e41f63f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://wilgnne.github.io/nlw-1/static/js/2.1e41f63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24f09e3c3793d70e407",
+    "url": "https://wilgnne.github.io/nlw-1/static/js/main.759cb07a.chunk.js"
+  },
+  {
+    "revision": "de7cb7a49124afb6a057",
+    "url": "https://wilgnne.github.io/nlw-1/static/js/runtime-main.592179e9.js"
+  },
+  {
+    "revision": "0f45e296a90e422f2d86af8b514de934",
+    "url": "https://wilgnne.github.io/nlw-1/static/media/home-background.0f45e296.svg"
+  },
+  {
+    "revision": "4abe42512d65710f6a898249bf23a254",
+    "url": "https://wilgnne.github.io/nlw-1/static/media/logo.4abe4251.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "nlw-1/precache-manifest.efef418f713bab12d1598c9a4600d833.js"
+  "https://wilgnne.github.io/nlw-1/precache-manifest.beed73c0c43d401b1477e053dfa2b6ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("nlw-1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://wilgnne.github.io/nlw-1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
