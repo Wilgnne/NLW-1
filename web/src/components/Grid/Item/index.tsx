529fdff41d98fedf04d10cabc6a62d339ddc6e97
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-06-07 03:49:44 -0300

    OnClick Item

--- web/src/components/Grid/Item/index.tsx
+++ web/src/components/Grid/Item/index.tsx
@@ -6,12 +6,16 @@ interface ItemProps {
   title: string,
   src?: string,
   alt?: string,
+  onClick?: ((event: React.MouseEvent<HTMLLIElement, MouseEvent>) => void) | undefined,
   selected?: boolean
 }
 
-const Item: React.FC<ItemProps> = ({ title, src, alt, selected }) => {
+const Item: React.FC<ItemProps> = ({ title, src, alt, selected, onClick }) => {
   return (
-    <Container className={selected ? 'selected' : undefined}>
+    <Container
+      className={selected ? 'selected' : undefined}
+      onClick={onClick}
+    >
       <img src={src} alt={alt} />
       <span>{title}</span>
     </Container>
