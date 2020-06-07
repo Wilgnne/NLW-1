8fca5510ce85bf920d27de05292c8106c6baf1d7
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-06-07 03:50:14 -0300

    Send Form

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { LeafletMouseEvent } from 'leaflet';
 
 import { Container } from './styles';
 
@@ -20,6 +22,10 @@ import api, { ApiItem } from '../../services/api';
 import { GetUFs, GetCitys } from '../../services/ibge';
 
 const CreatePoint: React.FC = () => {
+  const history = useHistory();
+
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+
   const [items, setItems] = useState<ApiItem[]>([]);
 
   const [ufs, setUFs] = useState<string[]>([]);
@@ -27,6 +33,25 @@ const CreatePoint: React.FC = () => {
 
   const [cities, setCities] = useState<string[]>([]);
   const [selectedCity, setSelectedCity] = useState<string>('0');
+
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: ''
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+
+      setInitialPosition([latitude, longitude]);
+    });
+  }, []);
 
   useEffect(() => {
     api.get('items').then(response => {
@@ -53,6 +78,55 @@ const CreatePoint: React.FC = () => {
     setSelectedCity(e.target.value);
   }
 
+  function handleMapClick(e: LeafletMouseEvent) {
+    setSelectedPosition([e.latlng.lat, e.latlng.lng]);
+  }
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.includes(id);
+
+    if (alreadySelected) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  }
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUF;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items
+    };
+
+    await api.post('points', data);
+    alert('Ponto de coleta cadastrado');
+
+    history.push('/');
+  }
+
   return (
     <Container>
       <Header
@@ -65,31 +139,39 @@ const CreatePoint: React.FC = () => {
         }}
       />
 
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
         <Fieldset title='Dados'>
           <Field
             name='name'
             type='text'
             labelText='Nome da entidade'
+            onChange={handleInputChange}
           />
           <FieldGroup>
             <Field
               name='email'
               type='email'
               labelText='E-mail'
+              onChange={handleInputChange}
             />
 
             <Field
               name='whatsapp'
               type='text'
               labelText='Whatsapp'
+              onChange={handleInputChange}
             />
           </FieldGroup>
         </Fieldset>
 
         <Fieldset title='Endereço' subtitle='Selecione o endereço no mapa'>
-          <Maps>
+          <Maps
+            center={initialPosition}
+            zoom={13}
+            markings={[selectedPosition]}
+            onClick={handleMapClick}
+          >
 
           </Maps>
           <FieldGroup>
@@ -115,13 +197,15 @@ const CreatePoint: React.FC = () => {
 
         <Fieldset title='Ítens de Coleta' subtitle='Selecione um ou mais ítens abaixo'>
           <Grid>
-            {items.map((item, index) =>
+            {items.map(item =>
               (
                 <Item
-                  key={index}
+                  key={item.id}
                   title={item.title}
                   src={item.image_url}
                   alt={item.title}
+                  selected={selectedItems.includes(item.id)}
+                  onClick={() => handleSelectItem(item.id)}
                 />
               )
             )}
